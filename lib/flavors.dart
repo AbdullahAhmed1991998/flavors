52bfba94cd41502811e76a023cb6b41eb6b4d695
abdullah <abdullahahmed1991998@gmail.com>
2024-12-24 17:50:48 +0300

    upload

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,44 +1,8 @@
 library flavors;
 
+export 'src/calculator.dart';
+export 'src/flavor_config.dart';
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-
-  int count = 0;
-
-  void increment() {
-    count++;
-  }
-
-  void decrement() {
-    count--;
-  }
-  static String flavor = 'dev';
-
-}
 
 
-class FlavorConfig {
-  int addOne(int value) => value + 1;
-  int count = 0;
 
-  void increment() {
-    count++;
-  }
-
-  void decrement() {
-    count--;
-  }
-
-}
-
-
-class Counterrr {
-  int count = 0;
-
-
-  void increment() {
-    count++;
-  }
-}
---
+++ lib/src/flavor_config.dart
@@ -0,0 +1,13 @@
+class FlavorConfig {
+  int addOne(int value) => value + 1;
+  int count = 0;
+
+  void increment() {
+    count++;
+  }
+
+  void decrement() {
+    count--;
+  }
+
+}
