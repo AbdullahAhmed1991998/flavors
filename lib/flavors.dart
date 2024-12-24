7fe50f8bba9645f0d7630e0b30ad381ddb3bc07e
abdullah <abdullahahmed1991998@gmail.com>
2024-12-24 18:29:52 +0300

    upload

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,8 +1,17 @@
-library flavors;
-
-export 'src/calculator.dart';
-export 'src/flavor_config.dart';
-/// A Calculator.
+import 'package:flutter/material.dart';
+class Test extends StatefulWidget {
 
 
+  final String title;
+  const Test({Key? key, required this.title}) : super(key: key);
 
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
